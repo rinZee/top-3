2f67854f0b2ca8a4d03729bcc1da1422b174e67e
tsheten renzee sherpa <tsheten@tshetens-MacBook-Pro.local>
2024-01-25 16:08:17 -0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,32 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <p>Placeholder for landing page</p>
+      <Image
+              src="/landing.jpg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+      <Image
+              src="/landing-2.jpg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+<Image
+              src="/Instagram.jpg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+      {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
@@ -108,6 +133,8 @@ export default function Home() {
           </p>
         </a>
       </div>
+
+  */}
     </main>
   );
 }
